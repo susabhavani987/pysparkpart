52a93ac3a04ef76e34aba65899bf0486cfb2a839
susabhavani987 <vijaychava101@gmail.com>
2025-09-05 17:09:58 -0400

    thanks

--- my_email_traverse.py
+++ my_email_traverse.py
@@ -18,11 +18,11 @@ data = [
      ("klice", "7038331668", "vijay2@yahoo.com"," "),
      ("Alice2", "", "vijay2@yahoo.com"," "),
 	  ("Alice3", "", "","vijay3@yahoo.com "),
-      ("Alice4", "", "","vijay4@yahoo.com ")
+      ("Alice4", "", "","")
 ]
 columns = ["Name", "phone", "email1", "email2"]
 
 df = spark.createDataFrame(data, columns)
+df_filtered = df.filter(col("phone").isNotNull() | col("email1").isNotNull() | col("email2").isNotNull() )
 
-
-df.show()
+df_filtered.show()

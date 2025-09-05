adf7a1d351438d5a4e5467ea75804255e1a880db
susabhavani987 <vijaychava101@gmail.com>
2025-09-05 14:44:37 -0400

    thanks

--- my_email_traverse.py
+++ my_email_traverse.py
@@ -14,10 +14,10 @@ spark = SparkSession.builder \
 
 # Sample DataFrame
 data = [
-    (Alice, "7038331668", "vijay1@yahoo.com","dj@yahoo.com "),
-     (klice, "7038331668", "vijay2@yahoo.com"," "),
-     (Alice2, "", "vijay2@yahoo.com"," ")
-	  (Alice3, "", "","vijay3@yahoo.com ")
+    ("Alice", "7038331668", "vijay1@yahoo.com","dj@yahoo.com "),
+     ("klice", "7038331668", "vijay2@yahoo.com"," "),
+     ("Alice2", "", "vijay2@yahoo.com"," ")
+	  ("Alice3", "", "","vijay3@yahoo.com ")
 ]
 columns = ["Name", "phone", "email1", "email2"]
 
